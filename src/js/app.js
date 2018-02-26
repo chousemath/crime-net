e9160d131ab4ebf3abf168b47cd502acd4b476d4
Joseph Sungpil Choi <chousemath@gmail.com>
2018-02-27 05:43:21 +0900

    First significant commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,8 +1,6 @@
-var App = {
+App = {
   web3Provider: null,
   contracts: {},
-  CaseContract: null,
-  Case: null,
 
   init: function() {
     $.getJSON('./js/config.json', function(config) {
@@ -11,77 +9,56 @@ var App = {
       // Initialize Cloud Firestore through Firebase
       var db = firebase.firestore();
     });
+
     return App.initWeb3();
   },
 
   initWeb3: function() {
-    // Check if there is an injected web3 instance
+    // Is there an injected web3 instance?
     if (typeof web3 !== 'undefined') {
       App.web3Provider = web3.currentProvider;
     } else {
-      // No injected web3 instance detected...
-      // Fall back on Ganache
+      // If no injected web3 instance is detected, fall back to Ganache
       App.web3Provider = new Web3.providers.HttpProvider('http://localhost:7545');
     }
     web3 = new Web3(App.web3Provider);
+
     return App.initContract();
   },
 
   initContract: function() {
-    $.getJSON('./contracts/Case.json', function(data) {
-      // Get contract artifact file
-      // Artifacts are information about our contract such as its
-      // deployed address and Application Binary Interface (ABI).
-      // The ABI is a JavaScript object defining how to interact
-      // with the contract including its variables, functions and their parameters
+    $.getJSON('Case.json', function(data) {
+      // Get the necessary contract artifact file and instantiate it with truffle-contract
       var CaseArtifact = data;
-      CaseContract = web3.eth.contract(data);
-      Case = CaseContract.at(0x855d1c79ad3fb086d516554dc7187e3fdfc1c79a);
-      console.log(CaseContract);
-      // Instantiate contract artifact with truffle-contract
-      // This keeps information about the contract in sync with migrations,
-      // so you don't need to change the contract's deployed address manually.
-      // This creates an instance of our contract we can interact with
       App.contracts.Case = TruffleContract(CaseArtifact);
+    
       // Set the provider for our contract
       App.contracts.Case.setProvider(App.web3Provider);
+      console.log('app', App.contracts.Case);
+      App.test();
     });
-
     return App.bindEvents();
   },
 
   bindEvents: function() {
-    $(document).on('click', '#create-case', App.handleCreateCase);
+    $(document).on('click', '.btn-adopt', App.handleAdopt);
   },
 
-  handleCreateCase: function(event) {
-    event.preventDefault();
+  test: function(adopters, account) {
     var caseInstance;
-    web3.eth.createCase(function(error, accounts) {
-      if (error) { console.log(error); }
-      var account = accounts[0];
-      App.contracts.Case.deployed().then(function(instance) {
-        caseInstance = instance;
-        // Execute adopt as a TRANSACTION by sending account
-        var title = 'test title';
-        var description = 'test description';
-        return caseInstance.createCase(title, description, description, { from: account });
-      }).then(function(result) {
-        console.log(result);
-      }).catch(function(err) {
-        console.log(err.message);
-      });
-    });
+
+    App.contracts.Case.deployed().then(function(instance) {
+      caseInstance = instance;
+      console.log('number:', caseInstance.numberOfCases().then(function(x) {
+        console.log('x', x);
+      }));
+    })
   }
 
 };
 
 $(function() {
-
-  $(document).ready(function() {
+  $(window).load(function() {
     App.init();
   });
 });
-
-
-
--- truffle.js
+++ truffle.js
@@ -1,9 +1,11 @@
 module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // for more about customizing your Truffle configuration!
   networks: {
     development: {
-    host: "localhost",
-    port: 7545,
-    network_id: "*" // Match any network id
-   }
+      host: "127.0.0.1",
+      port: 7545,
+      network_id: "*" // Match any network id
+    }
   }
 };
